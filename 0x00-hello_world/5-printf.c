9fdd770c10933a527b413b7d9ce9ce9ae56c9936
nellyxhathe <nellyxhathe2@gmail.com>
2023-06-17 09:21:46 -0700

    sometimes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints file using printf
+ * Description: prints "with proper grammar,but the outcome is a piece of art,"
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
