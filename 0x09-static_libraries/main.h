0a1562423b0703df7357ac49d7b826c5a766011c
nellyxhathe <nellyxhathe2@gmail.com>
2023-07-12 16:22:13 -0700

    my tasks

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -15,9 +16,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+
 #endif
