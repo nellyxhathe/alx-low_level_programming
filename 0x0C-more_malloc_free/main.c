9e150c58adc66f6dff5baa7b90596ada598e1bef
nellyxhathe <nellyxhathe2@gmail.com>
2023-07-14 19:12:36 -0700

    MALLOC

---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
+}
+ 15 changes: 15 additions & 0 deletions15  
+0x0C-more_malloc_free/0-malloc_checked.c
+@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to allocate
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
