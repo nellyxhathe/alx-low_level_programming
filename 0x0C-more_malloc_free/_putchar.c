be81f09453e064e5fb1210d5bdb3b6340fce8146
nellyxhathe <nellyxhathe2@gmail.com>
2023-07-14 20:06:35 -0700

    MALLOC

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -2,11 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
+ * @c: character to print
+ * Return: 1 (success), -1 (on error)
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
